e7efda4cc16c2f4b43cc78c02d6dcdc63883c38e
LilPeenieWeenie <jaadynhumphries@gmail.com>
2023-11-19 19:19:05 -0800

    better vim movement

--- init.lua
+++ init.lua
@@ -3,3 +3,12 @@ require("config.lazy")
 
 vim.api.nvim_set_option('clipboard', 'unnamedplus')
 
+-- Key mappings
+vim.api.nvim_set_keymap('n', '<Enter>', 'o <ESC>', {})
+vim.api.nvim_set_keymap('n', '<S-Enter>', 'O <ESC>', {})
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true })
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-c>', '"+y', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-v>', '"+p', { noremap = true })
